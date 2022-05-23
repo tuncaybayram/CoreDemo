35df079a022779713fed168935f7baedbbf762bb
tuncaybayram <tuncaybayrm9@gmail.com>
2022-05-24 02:29:42 +0300

    Register Page

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            p.WriterStatus = true;
+            p.WriterAbout = "Deneme Test";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
